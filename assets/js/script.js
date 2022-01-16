58f0ff18ac0451276e52175951e236f6e359f668
PaddyFA <paddyf85@hotmail.com>
2022-01-16 21:22:44 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,25 @@ document.addEventListener("DOMContentLoaded", function(){
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked submit");
             } else {
-                let gameType = this.getAttribute("data-type")
-                alert(`you clicked ${gameType}`);
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition")
 })
 
-function runGame() {
+function runGame(gameType) {
      let num1 = Math.floor(Math.random() * 25) + 1;
      let num2 = Math.floor(Math.random() * 25) + 1;
+
+     if (gameType === "addition") {
+         displayAdditionQuestion(num1, num2);
+     } else {
+         alert(`unknown game type: ${gameType}`);
+         throw `unknown game type: ${gameType}. Aborting!`;
+     }
 }
 
 function checkAnswer() {
@@ -34,8 +43,10 @@ function incrementWrongScore() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
